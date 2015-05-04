8a57c3e869e2fa66a6bb6ea5297f820d34da1304
VitorMakoto <makoto@work.co>
2015-05-04 09:57:30 -0400

    Initial commit

---
+++ cmake/CMakeFiles/smaa.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/vitormakoto/Developer/SMAA-OPENGL/smaa-opengl/main.cpp" "/Users/vitormakoto/Developer/SMAA-OPENGL/smaa-opengl/cmake/CMakeFiles/smaa.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../external/include"
+  "/usr/local/include"
+  "/usr/local/Cellar/glfw3/3.1.1/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ cmake/CMakeFiles/smaa.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smaa.dir/main.cpp.o"
+  "smaa.pdb"
+  "smaa"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smaa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
